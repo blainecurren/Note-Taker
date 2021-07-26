a5e59707d267e2c85db969b65d642e7815c35a7a
Blaine Curren <blaine.curren@gmail.com>
2021-07-25 22:46:36 -0500

    added get functionality

--- server.js
+++ server.js
@@ -16,3 +16,15 @@ app.use(
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.get("api/takenNotes", (req, res) => {
+  res.json(takenNotes.slice(1));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./dist/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./dist/note.html"));
+});
